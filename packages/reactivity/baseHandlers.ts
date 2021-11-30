550b7838ad681f13a9466b544d4f0dad58a205f1
yanjian <yanjian92@126.com>
2021-11-30 12:31:11 +0800

    完成reactivity结构

---
+++ packages/reactivity/baseHandlers.ts
@@ -0,0 +1,50 @@
+import { isObject } from "@vue/shared"
+import { reactive, readonly } from "./reactivity"
+
+function createGetter(isShallow = false, isReadonly = false) {
+  return function get(target, key, receiver) {
+    const res = Reflect.get(target, key, receiver)
+    if (isShallow) {
+      return res
+    }
+    if (isObject(res)) {
+      return isReadonly ? readonly(res) : reactive(res)
+    }
+
+    return res
+  }
+}
+function createSetter() {
+  return function set(target, key, value, receiver) {
+    const res = Reflect.set(target, key, value, receiver)
+
+    return res
+  }
+}
+const readonlyObj = {
+  set(target, key) {
+    console.log(`set fail on ${key}`)
+  }
+}
+
+const get = createGetter()
+const shallowGetter = createGetter(true, false)
+const readonlyGetter = createGetter(false, true)
+const shallowReadonlyGetter = createGetter(true, true)
+
+const set = createSetter()
+const shallowSetter = createSetter()
+export const mutableHandlers = {
+  get,
+  set,
+}
+export const shallowHandlers = {
+  get: shallowGetter,
+  set: shallowSetter,
+}
+export const readonlyHandlers = Object.assign({
+  get: readonlyGetter,
+}, readonlyObj)
+export const shallowReadonlyHandlers = Object.assign({
+  get: shallowReadonlyGetter
+}, readonlyObj)
--- packages/reactivity/reactivity.ts
+++ packages/reactivity/reactivity.ts
@@ -1,19 +1,31 @@
+import { isObject } from "@vue/shared";
+import { mutableHandlers, readonlyHandlers, shallowHandlers, shallowReadonlyHandlers } from "./baseHandlers";
+
 const reactiveMap = new WeakMap();
 const readonlyMap = new WeakMap();
-function createReactiveObject(target, isReadonly) {
-  const targetMap = isReadonly ? readonlyMap : readonlyMap;
+function createReactiveObject(target, isReadonly, baseHandlers) {
+  if (!isObject(target)) return
+  const targetMap = isReadonly ? readonlyMap : reactiveMap;
   if (targetMap.get(target)) {
     return targetMap.get(target);
   }
-  const proxy = new Proxy(target, {});
+  const proxy = new Proxy(target, baseHandlers);
   targetMap.set(target, proxy);
 
   return proxy;
 }
-export function reactive(target) {}
+export function reactive(target) {
+  return createReactiveObject(target, false, mutableHandlers)
+}
 
-export function shallowReactive(target) {}
+export function shallowReactive(target) {
+  return createReactiveObject(target, false, shallowHandlers)
+}
 
-export function readonly(target) {}
+export function readonly(target) {
+  return createReactiveObject(target, true, readonlyHandlers)
+}
 
-export function shallowReadonly(target) {}
+export function shallowReadonly(target) {
+  return createReactiveObject(target, true, shallowReadonlyHandlers)
+}
